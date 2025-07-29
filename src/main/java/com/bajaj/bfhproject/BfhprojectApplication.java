7e1d9d8cf74a138096c7ece43e1e35303bed4077
vivekgoyal2110 <122520555+vivekgoyal2110@users.noreply.github.com>
2025-07-29 20:21:38 +0530

    Initial Spring Boot setup with dependencies

---
+++ src/main/java/com/bajaj/bfhproject/BfhprojectApplication.java
@@ -0,0 +1,13 @@
+package com.bajaj.bfhproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfhprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BfhprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bajaj/bfhproject/BfhprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bajaj.bfhproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfhprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
